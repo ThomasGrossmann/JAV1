fec5e04fb82f726a559f4133b41e71b4ccc0dc48
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2023-06-22 21:23:38 +0200

    Add RecyclerView to display last 3 added books

---
+++ BookMyBook/app/src/main/java/com/example/bookmybook/ui/home/CardViewHolder.kt
@@ -0,0 +1,14 @@
+package com.example.bookmybook.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookmybook.databinding.CardCellBinding
+import com.example.bookmybook.models.Book
+
+class CardViewHolder (private val cardCellBinding: CardCellBinding) : RecyclerView.ViewHolder(cardCellBinding.root)
+{
+    fun bindBook(book: Book) {
+        cardCellBinding.textTitlePo.text = book.title
+        cardCellBinding.textAuthorPo.text = book.author
+        cardCellBinding.textIsbnPo.text = book.isbn
+    }
+}
--- BookMyBook/app/src/main/java/com/example/bookmybook/ui/home/HomeFragment.kt
+++ BookMyBook/app/src/main/java/com/example/bookmybook/ui/home/HomeFragment.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.bookmybook.DBHelper
 import com.example.bookmybook.databinding.FragmentHomeBinding
+import com.example.bookmybook.models.Book
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -21,13 +20,44 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val bookCountText = binding.textNbbooks
+        val rentCountText = binding.textNbrents
+        val db = DBHelper(requireContext(), null)
 
+        val bookCount = db.getBooks()
+        val rentCount = db.getRents()
+        val lastAddedBooks = db.getLastThreeBooks()
+        val lastThreeBooks = ArrayList<Book>()
+
+        lastAddedBooks?.use { cursor ->
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(cursor.getColumnIndex(DBHelper.BOOK_COLUMN_ID))
+                val title = cursor.getString(cursor.getColumnIndex(DBHelper.BOOK_COLUMN_TITLE))
+                val isbn = cursor.getString(cursor.getColumnIndex(DBHelper.BOOK_COLUMN_ISBN))
+                val author = cursor.getString(cursor.getColumnIndex(DBHelper.BOOK_COLUMN_AUTHOR))
+
+                val book = Book(title, isbn, author, id.toInt())
+                lastThreeBooks.add(book)
+            }
+        }
+
+        binding.recyclerLastBooks.apply {
+            layoutManager = GridLayoutManager(requireContext(), 3)
+            adapter = CardAdapter(lastThreeBooks)
+        }
+
+        if (bookCount != null) {
+            bookCountText.text = bookCount.count.toString()
+        }
+
+        if (rentCount != null) {
+            rentCountText.text = rentCount.count.toString()
+        }
+
+        db.close()
 
         return root
     }
